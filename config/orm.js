052fe2ae184118aefcb75f15b261aafb2b441ebb
Sama Sandy <samasandy86@gmail.com>
2018-05-19 22:23:43 -0500

    Adding in ORM and burgers.js files

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,38 @@
 let connection = require("../config/connection.js")
 
 let orm = {
-    all: function(tableInput, colToSearch, valOfCol) {
-        console.log("orm.all" )
-        // var queryString = "SELECT * FROM ";
-        // connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-        //   if (err) throw err;
-        //   console.log(result);
+    selectAll: function (tableInput, callback) {
+        //console.log("orm.all" )
+        connection.query(`SELECT * FROM ${tableInput};`, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+            console.log(result);
+        })
+    },
+    createBurger: function (table, burgerName, callback) {
+        connection.query(`INSERT INTO ${tableInput} SET ?`, {
+            burger_name: burgerName,
+            devoured: false,
+        }, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+            console.log(result);
+        })
+    },
+    updateBurger: function (table, burgerID, callback) {
+        connection.query(`INSERT INTO ${tableInput} SET ? WHERE`, [{ devoured: true }, { id: burgerID }], function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                callback(result);
+                console.log(result);
+            })
     }
-    };
+};
 
 
-
-
-
-  
 module.exports = orm;
-  
---
+++ models/burger.js
@@ -0,0 +1,25 @@
+let orm = require("./config/orm.js");
+
+
+var burger = {
+    selectAll: function(callback) {
+      orm.selectAll("burgers", function(res) {
+        callback(res);
+      });
+    },
+    createBurger: function(burgerName, callback){
+        orm.createBurger("burgers", burgerName, function(res){
+        callback(res);
+        })
+    },
+    updateBurger: function(burgerID, callback){
+        orm.updateBurger("burgers", burgerID, function(res){
+            callback(res);
+        })
+    },
+    delete: function(condition, callback) {
+        orm.delete("burgers", condition, function(res) {
+          cb(res);
+        });
+    }
+}
